7e858f515620585d94112e87ce286cc1d96adf4f
bregloomy16 <bregloomy16@qq.com>
2023-03-26 11:19:00 +0800

    完善骰子功能

--- trpg.py
+++ trpg.py
@@ -10,17 +10,23 @@ def roll(raw_card, raw_message):
         return False
     if x != 1:
         judge = False
-    while x:
+        i = x
+        while i:
+            res = random.randint(1, y)
+            text += str(res)
+            if x != 1:
+                text += "   "
+            i -= 1
+            z += res
+
+    else:
         res = random.randint(1, y)
         if y == 100 and res > 95:
-            text = "大失败！"
-        if y == 100 and res == 1:
-            text = "大成功！"
-        text += str(res)
-        if x != 1:
-            text += "   "
-        x -= 1
-        z += res
+            text = "%s, 大失败！" % res
+        elif y == 100 and res == 1:
+            text = "%s, 大成功！" % res
+        else:
+            text = "%s" %res
     text = raw_card + "的投掷结果为：" + text
     if judge is False:
         text += "   总计：%s" % z
