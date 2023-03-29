352cc435fd8569fe497e18a822f2f19f26366549
bregloomy16 <bregloomy16@qq.com>
2023-03-29 19:51:50 +0800

    调整kfc提醒代码

--- KFC_remind.py
+++ KFC_remind.py
@@ -3,7 +3,7 @@ kfc_status = 0
 def kfc(hour, minute, week):
     global kfc_status
     if week == 3 and hour == 8 and minute == 0 and kfc_status == 0:
-        text = "今天是疯狂星期四，请全体群员vivo50，望周知"
+        text = "今天是疯狂星期四，请全体群员vivo50，望周知❤"
         kfc_status = 1
         return text
 
