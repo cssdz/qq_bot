dec2bcbdfe2fe1509b7db627b1e0bec5f47c7f1a
bregloomy16 <bregloomy16@qq.com>
2023-04-19 19:51:45 +0800

    增加暗骰功能

--- main_server.py
+++ main_server.py
@@ -13,9 +13,9 @@ import KFC_remind
 import random_attribute
 
 app = Sanic('qqbot')
-switch = False
 at = '[CQ:at,qq=1154850482]'
 reply_list = ['在？', '在不？', '在吗？', '滴滴', '？']
+switch = False
 
 
 @app.websocket('/qqbot')
@@ -38,8 +38,6 @@ async def qqbot(request, ws):
 
             # print(data)
             ret = group_message(raw_message, time_, raw_card, raw_nickname, group_id, user_id)
-            if ret is False:
-                continue
             await ws.send(json.dumps(ret))
 
         if data.get('message_type') == 'private' and data.get('raw_message'):
@@ -49,8 +47,12 @@ async def qqbot(request, ws):
             user_id = data['user_id']
             time_ = int(data['time'])
 
-            ret = private_message(raw_message, time_, raw_nickname, user_id)
+            ret, mode = private_message(raw_message, time_, raw_nickname, user_id)
             await ws.send(json.dumps(ret))
+            if mode == "trpg":
+                text, group_id = "kp进行了暗骰！", 386781905
+                ret = group_ret(group_id, text)
+                await ws.send(json.dumps(ret))
 
         else:
             status = 0
@@ -113,19 +115,39 @@ def group_ret(group_id, text):
 
 # 个人消息
 def private_message(raw_message, time_, nickname, user_id):
+    global switch
+    mode, text = "normal", ""
+
+    # 开启骰子功能
+    if raw_message == "trpg on":
+        switch = True
+        text = "GET READY"
+
+    # 关闭骰子功能
+    if raw_message == "trpg off":
+        switch = False
+        text = "SYSTEM OVER"
+
+    if switch is True and raw_message.find('d') > 0:
+        text, sum_ = trpg.roll(nickname, raw_message)
+        if text is not False:
+            user_id = 3271993008
+            mode = "trpg"
+            ret = private_ret(user_id, text)
+            return ret, mode
+
     if raw_message in reply_list:
         text = '【自动回复】本人现在不在，有事请留言，最好不要以表情包和图片的形式告知。'
-    else:
+    if user_id != user_id:
         text = '【滴滴】' + nickname + '(' + str(user_id) + ')：' + raw_message
         user_id = 3271993008
     ret = private_ret(user_id, text)
-    return ret
+    return ret, mode
 
 
 # 群消息
 def group_message(raw_message, time_, raw_card, raw_nickname, group_id=771695831, user_id=None):
     text = ""
-    global switch
     # 群昵称替换QQ名
     if raw_card == "":
         raw_card = raw_nickname
@@ -154,28 +176,22 @@ def group_message(raw_message, time_, raw_card, raw_nickname, group_id=771695831
     # 翻译功能
     if "fy " in raw_message:
         info = raw_message[3:]
-        print(info)
         text = translate.connect(info)
 
+    # 创建角色属性
     if raw_message == "roll role":
         text = random_attribute.rand_attribute(raw_card)
 
-    # 开启骰子功能
-    if raw_message == "trpg on":
-        switch = True
-        text = "GET READY"
-
-    # 关闭骰子功能
-    if raw_message == "trpg off":
-        switch = False
-        text = "SYSTEM OVER"
-
     # 骰子功能体现
-    if switch is True and raw_message.find('d') > 0:
-        text, sum_ = trpg.roll(raw_card, raw_message)
+    if raw_message.find('d') > 0:
+        try:
+            text, sum_ = trpg.roll(raw_card, raw_message)
+        except TypeError:
+            pass
         if text is False:
             return
 
+    # 看板功能
     if "project" in raw_message and group_id == 771695831:
         try:
             no = int(raw_message[7:])
