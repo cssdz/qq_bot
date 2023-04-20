4ed52874148924b6c274641f8aa5d2dde39a9107
bregloomy16 <bregloomy16@qq.com>
2023-04-20 18:34:40 +0800

    修改KFC的bug

--- KFC_remind.py
+++ KFC_remind.py
@@ -2,7 +2,8 @@ kfc_status = 0
 
 def kfc(hour, minute, week):
     global kfc_status
-    if week == 3 and hour == 8 and minute == 0 and kfc_status == 0:
+    if week == 3 and hour == 18 and minute == 30 and kfc_status == 0:
+        print("test")
         text = "今天是疯狂星期四，请全体群员vivo50，望周知❤"
         kfc_status = 1
         return text
